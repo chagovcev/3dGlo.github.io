abf5d54a3b02210213b22330d4d17bc40feeff06
Roman Chagovtsev <4agovtsev@gmail.com>
2020-03-11 19:38:02 +0100

    промисы и исчезновение сообщения

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -1,9 +1,10 @@
 const sendForm = () => {
     const errorMessage = 'Что-то пошло не так...',
-        loadMessage = 'Загрузка',
-        successMessage = 'Спасибо мы скоро с вами свяжемся!';
+        loadMessage = 'Загрузка...',
+        successMessage = 'Спасибо мы скоро с Вами свяжемся!';
     
-    const form = document.querySelectorAll('form'),
+    const form = document.querySelectorAll('form1'),
+        myForm = document.getElementById('form1'),
         allInputs = document.querySelectorAll('input');
         
 
@@ -28,73 +29,107 @@ const sendForm = () => {
     const statusMessage = document.createElement('div');
     statusMessage.style.cssText = 'font-size: 2rem';
 
-    for(let i = 0; i < form.length; i++){
-        form[i].addEventListener('submit', (event) => {
-            event.preventDefault();
-            form[i].appendChild(statusMessage);               
-            statusMessage.textContent = loadMessage;
-            const formData = new FormData(form[i]); // все что содержится в форме и имеет аттрибут name!!
-            let body = {};
+    myForm.addEventListener('submit', (event) => {
 
-            // // for(let val of formData.entries()){
-            // //     body[val[0]] = val[1];
-            // // }
+        event.preventDefault();
+
+        myForm.appendChild(statusMessage);               
+        statusMessage.textContent = loadMessage;
+
+        const formData = new FormData(myForm);
+        
+        let body = {};
 
             formData.forEach((val, key) => {
-                body[key] = val;
+                        body[key] = val;
             });  
 
+            
+    console.log('body: ', body);            
+    });
+
+    // for(let i = 0; i < form.length; i++){
+    //     form[i].addEventListener('submit', (event) => {
+
+    //         event.preventDefault();
+
+    //         form[i].appendChild(statusMessage);               
+    //         statusMessage.textContent = loadMessage;
+    //         const formData = new FormData(form[i]); // все что содержится в форме и имеет аттрибут name!!
+    //         console.log('formData: ', formData);
+    //         let body = {};
+
+    //         // // for(let val of formData.entries()){
+    //         // //     body[val[0]] = val[1];
+    //         // // }
+
+    //         formData.forEach((val, key) => {
+    //             body[key] = val;
+    //             console.log('body: ', body);
+    //         });  
+
+                      
+    //     });
+    // }        
+
+    const postData = (body) => {
+        
+        return new Promise ((resolve, reject) => {
+            const request = new XMLHttpRequest();
+            request.addEventListener('readystatechange', () => {               
+
+                if(request.readyState !==4){
+                    return;
+                }
+    
+                if(request.status === 200){
+                    const response = JSON.parse(request.responseText);
+                    resolve(response);                   
+                } else {
+                    reject(request.statusText);                    
+                }
+    
+            });
+
+            request.open('POST', './server.php');
+            request.setRequestHeader('Content-type', 'application/json');
+                        
+            request.send(JSON.stringify(body));
+        });   
+        
+        
+        
+    //     // return fetch('./server.php', {
+    //     //     method: 'POST',
+    //     //     headers: {
+    //     //         'Content-type': 'application/json'
+    //     //     },
+    //     //     body: JSON.stringify(formData)
+    //     // });
+    // };
+
+};
             postData(body)
                 .then((response) => {
                     if (response.status !== 200){
                         throw new Error('status network not 200');
                     }
                     statusMessage.textContent = successMessage;
+                    setTimeout(() => {
+                        statusMessage.textContent = '';
+                    }, 5000);
+
                     for(let i = 0; i < allInputs.length; i++){
                         allInputs[i].value = '';
                     }
                 })
                 .catch((error) => {
                     statusMessage.textContent = errorMessage;
-                    console.error(error); 
-                });           
-        });
-    }        
-
-    const postData = (formData) => {
-
-        return fetch('./server.php', {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: JSON.stringify(formData)
-        });
-
-        // return new Promise ((resolve, reject) => {
-        //     const request = new XMLHttpRequest();
-        //     request.addEventListener('readystatechange', () => {               
-
-        //         if(request.readyState !==4){
-        //             return;
-        //         }
-    
-        //         if(request.status === 200){
-        //             const response = JSON.parse(request.responseText);
-        //             resolve(response);                   
-        //         } else {
-        //             reject(request.statusText);                    
-        //         }
-    
-        //     });
-
-        //     request.open('POST', './server.php');
-        //     request.setRequestHeader('Content-type', 'application/json');
-                        
-        //     request.send(JSON.stringify(body));
-        // });            
-    };
-
-};
-
+                    setTimeout(() => {
+                        statusMessage.textContent = '';
+                    }, 5000);
+                    
+                    console.error(error);                                         
+                }); 
+            }
 export default sendForm;
