1a3859b226bf7b71667205dd1e6b57e7f66e1c03
Roman Chagovtsev <4agovtsev@gmail.com>
2020-03-11 20:25:15 +0100

    фитчи

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -3,8 +3,7 @@ const sendForm = () => {
         loadMessage = 'Загрузка...',
         successMessage = 'Спасибо мы скоро с Вами свяжемся!';
     
-    const form = document.querySelectorAll('form1'),
-        myForm = document.getElementById('form1'),
+    const form = document.getElementById('form1'),
         allInputs = document.querySelectorAll('input');
         
 
@@ -29,23 +28,20 @@ const sendForm = () => {
     const statusMessage = document.createElement('div');
     statusMessage.style.cssText = 'font-size: 2rem';
 
-    myForm.addEventListener('submit', (event) => {
-
+    form.addEventListener('submit', (event) => {
         event.preventDefault();
 
-        myForm.appendChild(statusMessage);               
+        const formData = new FormData(form);
+        let body = {};        
+
+        form.appendChild(statusMessage);               
         statusMessage.textContent = loadMessage;
 
-        const formData = new FormData(myForm);
-        
-        let body = {};
-
-            formData.forEach((val, key) => {
-                        body[key] = val;
-            });  
-
+        formData.forEach((val, key) => {
+            body[key] = val;
+        });     
             
-    console.log('body: ', body);            
+        console.log('body: ', body);            
     });
 
     // for(let i = 0; i < form.length; i++){
@@ -72,64 +68,36 @@ const sendForm = () => {
     //     });
     // }        
 
-    const postData = (body) => {
-        
-        return new Promise ((resolve, reject) => {
-            const request = new XMLHttpRequest();
-            request.addEventListener('readystatechange', () => {               
+    const postData = formData => fetch(`./server.php`, {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    });
 
-                if(request.readyState !==4){
-                    return;
-                }
-    
-                if(request.status === 200){
-                    const response = JSON.parse(request.responseText);
-                    resolve(response);                   
-                } else {
-                    reject(request.statusText);                    
-                }
-    
-            });
-
-            request.open('POST', './server.php');
-            request.setRequestHeader('Content-type', 'application/json');
-                        
-            request.send(JSON.stringify(body));
-        });   
-        
-        
-        
-    //     // return fetch('./server.php', {
-    //     //     method: 'POST',
-    //     //     headers: {
-    //     //         'Content-type': 'application/json'
-    //     //     },
-    //     //     body: JSON.stringify(formData)
-    //     // });
-    // };
-
-};
-            postData(body)
-                .then((response) => {
-                    if (response.status !== 200){
-                        throw new Error('status network not 200');
-                    }
-                    statusMessage.textContent = successMessage;
-                    setTimeout(() => {
-                        statusMessage.textContent = '';
-                    }, 5000);
-
-                    for(let i = 0; i < allInputs.length; i++){
-                        allInputs[i].value = '';
-                    }
-                })
-                .catch((error) => {
-                    statusMessage.textContent = errorMessage;
-                    setTimeout(() => {
-                        statusMessage.textContent = '';
-                    }, 5000);
-                    
-                    console.error(error);                                         
-                }); 
+    postData(body)
+        .then(response => {
+            if (response.status !== 200) {
+                throw new Error('status network not 200');
             }
+            statusMessage.textContent = successMessage;
+            setTimeout(() => {
+                statusMessage.textContent = '';
+            }, 5000);
+
+            for(let i = 0; i < allInputs.length; i++){
+                allInputs[i].value = '';
+            }
+                    
+            form.reset();
+        })
+        .catch(error => {
+            statusMessage.textContent = errorMessage;
+            setTimeout(() => {
+                statusMessage.textContent = '';
+            }, 5000);        
+            console.error(error);
+        });
+};            
 export default sendForm;
